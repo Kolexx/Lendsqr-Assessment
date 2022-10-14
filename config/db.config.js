9b55913a0ffde638b8a75fb3be37c35331cbb9ce
Korede <solomonkorede15@gmail.com>
2022-10-14 14:17:13 +0100

    REMOVE CREATE TABLE

--- config/db.config.js
+++ config/db.config.js
@@ -27,19 +27,5 @@ knex
   .finally(() => {
     knex.destroy();
   });
-// knex.schema
-//   .createTable('account', (table) => {
-//     table.increments('acID');
-//     table.string('acNm');
-//     table.integer('balance ');
-//   })
-//   .then(() => console.log('table created'))
-//   .catch((err) => {
-//     console.log(err);
-//     throw err;
-//   })
-// .finally(() => {
-//   knex.destroy();
-// });
 
 module.exports = knex;
